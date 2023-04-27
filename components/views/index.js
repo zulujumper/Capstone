f20b9e444fca8dced4915a488877b8fb27d15ea0
zulujumper <zulujumper@gmail.com>
2023-04-27 13:05:06 -0700

    running with parcel, homework ready

--- components/views/index.js
+++ components/views/index.js
@@ -1,1 +1,5 @@
+export { default as About } from "./About";
+export { default as Home } from "./Home";
+export { default as Contact } from "./Contact";
+export { default as Results } from "./Results";
 
