5b4a8c0ada2187c46befade636fb6745fae4cb72
zulujumper <zulujumper@gmail.com>
2023-05-01 12:06:48 -0700

    axios

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { Header, Nav, Main, Footer } from "./components";
 import * as store from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
+import axios from "axios";
 
 const router = new Navigo("/");
 
