996eff776f69aceeafd81bb469906258985c4fce
zulujumper <zulujumper@gmail.com>
2023-05-02 15:22:05 -0700

    css work

--- components/views/Home.js
+++ components/views/Home.js
@@ -3,7 +3,7 @@ import html from "html-literal";
 export default state => html`
   <section id="jumbotron">
     <h1>EggBuddy</h1>
-    <input type="number" placeholder="Zip code"></input>
+    <input id="zip" type="number" placeholder="Zip code"></input>
     <a href="./Results">FIND ME THE EGGS!</a>
   </section>
   <p>${state.message}</p>
