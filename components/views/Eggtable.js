740508b11bdfb7bbc962f117c7808fb54a8bedd3
zulujumper <zulujumper@gmail.com>
2023-05-25 12:54:23 -0700

    pictures

--- components/views/Eggtable.js
+++ components/views/Eggtable.js
@@ -7,15 +7,17 @@ export default state => html`
     <tr>
       <th>Brand</th>
       <th>Quantity</th>
+      <th>       </th>
       <th>Stock Level</th>
       <th>Price</th>
     </tr>
     ${state.products
       .map(product => {
-        return `<tr><td class="brandName">${product["brand"]}</td>
-        <td class="quantity">${product["items"][0]["size"]}</td>
-        <td class="available">${product.items[0].inventory.stockLevel}</td>
-        <td class="price">${product["items"][0]["price"]["regular"]}</td>
+        return `<tr><td class="brandName">${product.brand}</td>
+        <td class="quantity">${product?.items[0]?.size}</td>
+        <td align="center" class="eggpic"><img src="${product?.images[0]?.sizes[2]?.url}" id="eggpic"></td>
+        <td class="available">${product?.items[0]?.inventory?.stockLevel}</td>
+        <td class="price">$${product?.items[0]?.price?.regular}</td>
         </tr>`;
       })
       .join("")}
